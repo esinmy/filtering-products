dd23bd68061db3e17322d0032509e04b5da99957
Maxim Esin <mesin@micromine.com.au>
2020-04-11 20:52:28 +0300

    liked_by field added

--- property/admin.py
+++ property/admin.py
@@ -4,11 +4,12 @@ from .models import Flat, Complaint
 
 
 class FlatAdmin(admin.ModelAdmin):
-    readonly_fields = ("created_at", )
+    readonly_fields = ("created_at",)
     search_fields = ("town", "address", "owner")
     list_display = ("address", "price", "new_building", "construction_year", "town")
     list_editable = ("new_building",)
     list_filter = ("new_building", "rooms_number", "has_balcony")
+    raw_id_fields = ("liked_by",)
 
 
 class ComplaintAdmin(admin.ModelAdmin):
