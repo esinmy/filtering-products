1159c3c38a430a6527bd41275153d3d4c65c05b9
Maxim Esin <mesin@micromine.com.au>
2020-04-11 14:52:56 +0300

    A little bit of refactoring

--- property/admin.py
+++ property/admin.py
@@ -2,15 +2,18 @@ from django.contrib import admin
 
 from .models import Flat, Complaint
 
+
 class FlatAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_at', )
+    readonly_fields = ("created_at", )
     search_fields = ("town", "address", "owner")
     list_display = ("address", "price", "new_building", "construction_year", "town")
-    list_editable = ("new_building", )
+    list_editable = ("new_building",)
     list_filter = ("new_building", "rooms_number", "has_balcony")
+
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ("user", "flat")
+
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
